02c90d5221650f2b4daf9b80e1d71f7cfff8af87
José Nelson Cultri <josefcultrifrc@gmail.com>
2022-06-09 10:32:05 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/josenelson/course/services/UserService.java
@@ -0,0 +1,25 @@
+package br.com.josenelson.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.josenelson.course.entities.User;
+import br.com.josenelson.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
