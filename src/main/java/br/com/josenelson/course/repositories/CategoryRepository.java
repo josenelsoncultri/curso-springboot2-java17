8e8226d92c16510f93eb5f70c8cfa2f453afda72
José Nelson Cultri <josefcultrifrc@gmail.com>
2022-06-10 09:01:35 -0300

    Category

---
+++ src/main/java/br/com/josenelson/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package br.com.josenelson.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.josenelson.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
